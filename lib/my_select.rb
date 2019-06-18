4e3f35b672e70d603c6999c30ead3fb498cdbab4
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 20:14:11 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  count = 0
+  newArray = []
+  while count < collection.length do
+    if (yield collection[count])
+      newArray.push(collection[count])
+    end
+    count += 1
+  end
+  return newArray
 end
