f1a13f71f3046139834e57137fe3a0f8483f5bf0
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 20:10:52 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
  # your code here!
+ count = 0
+ while count < collection.length
+   if yield collection[count]
+     newArray.push(collection[count])
+   end
+   count += 1
+ end
+ return newArray
 end
